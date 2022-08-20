2b71a4314662b1ae3dd91624d9b6801a37590f2d
Sean <scchalms@gmail.com>
2022-08-20 18:17:44 -0500

    chore: initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route, useParams, BrowserRouter} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/:text" element={<Text/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+function Text() {
+    let params = useParams();
+    return <div className={"blinker"}>
+        {params.text}
     </div>
-  );
+
 }
 
 export default App;
